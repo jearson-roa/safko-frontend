b6bb8a8b0a36ae547069ed0da2e56f37bb8540b1
Jearson Roa <jearson.roa@gmail.com>
2026-08-11 22:30:56 -0400

    Cambio de ruta en login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Loading from "../components/loading";
+import Loading from "../components/Loading";
 import "/src/App.css";
 
 function Login() {
